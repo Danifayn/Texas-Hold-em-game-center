9bd9f3f9e690b1546c562279666b7248d7c44d06
matantsu <matantsuberi@gmail.com>
2017-04-09 15:52:04 +0300

    added typescript support

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,6 @@
-var functions = require('firebase-functions');
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// })
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+exports.__esModule = true;
+__export(require("./build/index"));
