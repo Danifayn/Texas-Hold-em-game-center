87b8abead053c9c7d1464e319188a4dba363ced5
matantsu <matantsuberi@gmail.com>
2017-04-21 14:40:45 +0300

    improved service layer a bit

--- functions/src/game-center.ts
+++ functions/src/game-center.ts
@@ -1,7 +1,21 @@
+import * as assign from 'object.assign';
+
+export class Game {
+
+}
 export class GameCenter {
     private num: number = 234;
+    private games: Game[] = [];
 
     add(param: number) {
         this.num += +param;
+        return this.num;
+    }
+
+    // factory method to create a GameCenter instance from the json data from the db
+    public static from(json: any): GameCenter{
+        let gc = assign(new GameCenter(), json);
+        gc.games = gc.games.map(game => assign(new Game(), game));
+        return gc;
     }
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,10 @@
 import { GameCenter } from './game-center';
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
-var assign = require('object.assign');
+import * as assign from 'object.assign';
 admin.initializeApp(functions.config().firebase);
 
-type Extractor = {
+export type Extractor = {
   string: (name: string) => string;
   number: (name: string) => number;
   boolean: (name: string) => boolean;
@@ -13,34 +13,42 @@ type Extractor = {
 const createExtractor = (o: any): Extractor => ({
   string: name => {
     if(name in o)
-      return o[name];
+      return '' + o[name];
     else
-      throw new Error(`param ${name} not provided`);
+      throw new Error(`param '${name}' not provided`);
   },
   number: name => {
-    if(name in o && !isNaN(Number(o.name)))
+    if(name in o && !isNaN(o[name]))
       return +o[name];
     else
-      throw new Error(`param ${name} not provided or not a number`);
+      throw new Error(`param '${name}' not provided or not a number`);
   },
   boolean: name => o[name] ? true : false
 });
 
 const createHandler = (f: (gc: GameCenter, params: Extractor) => any) => {
   return functions.https.onRequest((req, res) => {
-    return admin.database().ref().transaction(db => {
+    var result;
+    var error: Error;
+    const onComplete = (e,commited,snapshot) => {
+      if(commited){
+        res.send({result});
+      } else {
+        res.send(error.message);
+      }
+    }
+    return admin.database().ref('/').transaction(db => {
       try{
-        let gc: GameCenter = assign(new GameCenter(), db);
-        let params = assign({},req.query, req.params, req.body);
-        f(gc, createExtractor(params));
-        res.status(200).end();
+        const gc = GameCenter.from(db);
+        const params = assign({},req.query, req.params, req.body);
+        result = f(gc, createExtractor(params));
         return gc;
       }
       catch(e){
-        res.status(400).send(e.message).end();
+        error = e;
         return;
       }
-    });
+    },onComplete);
   })
 }
 
